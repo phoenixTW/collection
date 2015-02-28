4b28bacc6c9da57d975fff6e8d5c26535f61ce0d
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-02-28 10:07:59 +0530

    v0.1 | scaffolds ready

---
+++ runTest.sh
@@ -0,0 +1,16 @@
+echo 'cleaning..'
+rm -f *.class
+echo "compiling..."
+javac -cp junit-4.10.jar *.java
+if [ $? != 0 ] 
+	then exit
+fi
+echo "running tests.."
+
+echo "------------------ACCEPTANCE TEST--------------------------------"
+java -cp ".;junit-4.10.jar" org.junit.runner.JUnitCore CollectionUtilsTest
+echo $?
+
+echo "--------------------- COMPLETED ---------------------------------"
+
+rm -f *.class
